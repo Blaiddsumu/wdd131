7383feedb723411776ffc774a52158e92d4ff58e
morgan <blaiddsumu@gmail.com>
2024-07-11 13:29:55 -0600

    hourgh

---
+++ dragons/scripts/dragons.js
@@ -0,0 +1,92 @@
+const dragons = [
+    {
+        type: "Conventional",
+        subtype: "Light",
+        name: "Mountain Red",
+        // this is for percy and quincy
+        info: "blah blah info here",
+        imageMain: "resources/dragon_images/quincy.png",
+        image2: ""
+    },
+    {
+        type: "Conventional",
+        subtype: "Light",
+        name: "Forest Grey",
+        info: "eepougghehhgld this is Storm",
+        imageMain: "resoureces/dragon_images/storm_scene.png",
+        image2: "resources/dragon_images/storm.png"
+    },
+    {
+        type: "Conventional",
+        subtype: "Heavy",
+        name: "Plains Strider",
+        info: "this totally isn't just flight rising snapper",
+        imageMain: "resources/dragon_images/snapper.png",
+        image2: ""
+    },
+    {
+        type: "Conventional",
+        subtype: "Heavy",
+        name: "",
+        info: "the alphyyyyy",
+        imageMain: "resources/dragon_images/alphys_scene.png",
+        image2: ""
+    },
+    {
+        type: "Unconventional",
+        subtype: "Aquatic",
+        name: "LandWalker",
+        info: "undyneeeeeeeee",
+        imageMain: "resources/dragon_images/undyne.png",
+        image2: "resources/dragon_images/undyne_head.png"
+    },
+    {
+        type: "Unconventional",
+        subtype: "Non-Aquatic",
+        name: "Forest Goliath",
+        info: "fern my beloved. hes a big boy",
+        imageMain: "resources/draogn_images/fern.png",
+        image2: ""
+    },
+    {
+        type: "Unconventional",
+        subtype: "Non-Aquatic",
+        name: "Cave-Settler",
+        info: "queen.",
+        imageMain: "resources/dragon_images/queen.png",
+        image2: ""
+    },
+    {
+        type: "Unconventional",
+        subtype: "Non-Aquatic",
+        name: "Mountain Goliath",
+        info: "wings. the. fern bro",
+        imageMain: "resources/dragon_images/wings.png",
+        image2: ""
+    },
+    {
+        type: "Unconventional",
+        subtype: "Aquatic",
+        name: "Leviathan",
+        info: "the leviathan...",
+        imageMain: "resources/dragon_images/leviathan.png",
+        image2: ""
+    },
+    {
+        type: "Unconventional",
+        subtype: "Aquatic",
+        name: "Guidewind",
+        info: "guideys :D",
+        imageMain: "resources/dragon_images/guides.png",
+        image2: ""
+    }
+    // ,{
+    //     type: "",
+    //     subtype: "",
+    //     name: "",
+    //     info: "",
+    //     imageMain: "",
+    //     image2: ""
+    // }
+    // etc
+]

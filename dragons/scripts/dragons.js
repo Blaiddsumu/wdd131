63a4899bf4607899c905f884c9747a41d77f4c8e
morgan <blaiddsumu@gmail.com>
2024-07-13 23:50:02 -0600

    random dragons added :)

--- dragons/scripts/dragons.js
+++ dragons/scripts/dragons.js
@@ -1,15 +1,15 @@
 const dragons = [
     {
         type: "Conventional",
-        subtype: "Light",
+        subtype: "light",
         name: "Mountain Red",
-        info: "Medium sized dragon. Its scales are various shades of reds, ranging from dark to light and anything in between. They have tusks located just underneath the eyes, and are capable of breathing fire. Their large wings enable them to soar well, and can often be spotted flying through the canyons of their natural mountainous habitat. Excellent hunters, they can easily hunt far more prey than hey need, which leads to a more social behavior compared to other dragons.",
+        info: "Medium sized dragon. Its scales are various shades of reds, ranging from dark to light and anything in between. They have tusks located just underneath the eyes, and are capable of breathing fire. Their large wings enable them to soar well, and can often be spotted flying through the canyons of their natural mountainous habitat. Excellent hunters, they can easily hunt far more prey than they need, which leads to a more social behavior compared to other dragons.",
         imageMain: "resources/dragon_images/quincy.png",
         image2: ""
     },
     {
         type: "Conventional",
-        subtype: "Light",
+        subtype: "light",
         name: "Forest Grey",
         info: "Small sized dragon. They're only slightly taller than the average human. Covered in a coat of fur and feathers, they're more likely to be found on the ground, weaving between the trees, than they are in the air. Generally solitary, but it's not unusual to find packs of them as well.",
         imageMain: "resources/dragon_images/storm_scene.jpg",
@@ -17,7 +17,7 @@ const dragons = [
     },
     {
         type: "Conventional",
-        subtype: "Heavy",
+        subtype: "heavy",
         name: "Plains Strider",
         info: "Small sized dragon. Incapable of flight. These dragons can be found in large family groups, and have an omnivorous diet. Their main habitat is in open fields and sparse forests, and they will eat anything from the plants and fruits around them, to small and medium sized forest animals.",
         imageMain: "resources/dragon_images/snapper.png",
@@ -25,15 +25,15 @@ const dragons = [
     },
     {
         type: "Conventional",
-        subtype: "Heavy",
-        name: "ksldjflksdjflksjd",
+        subtype: "heavy",
+        name: "Lightning Flier",
         info: "Medium sized dragon. These dragons build up static and can use it as an elemental attack. Capable of flight, though they may prefer to stay on the ground and within caves.",
         imageMain: "resources/dragon_images/alphys_scene.jpg",
         image2: ""
     },
     {
         type: "Unconventional",
-        subtype: "Aquatic",
+        subtype: "aquatic",
         name: "LandWalker",
         info: "Medium sized dragon. Capable of living on both land and water. Their legs are built for relatively efficient walking as well as swimming, and they have both gills as well as lungs. They have large, jagged teeth they use to latch onto prey.",
         imageMain: "resources/dragon_images/undyne.png",
@@ -41,7 +41,7 @@ const dragons = [
     },
     {
         type: "Unconventional",
-        subtype: "Non-Aquatic",
+        subtype: "nonaquatic",
         name: "Forest Goliath",
         info: "Gigantic sized dragon. This dragon has three pairs of legs; one pair of what could be considered arms, forelegs, and hindlegs. Despite its size, it is still capable of flight. These dragons may be found in ancient forests and near the foothills of mountains.",
         imageMain: "resources/dragon_images/fern.png",
@@ -49,7 +49,7 @@ const dragons = [
     },
     {
         type: "Unconventional",
-        subtype: "Non-Aquatic",
+        subtype: "nonaquatic",
         name: "Cave-Settler",
         info: "Small sized dragon. Its wings are diaphanous, visually similar to insect wings. These dragons can be found within large cave systems and caverns, though they are known to surface at night. Particularly curious ones may be found at the edges of human settlements, investigating the people and buildings.",
         imageMain: "resources/dragon_images/queen.png",
@@ -57,7 +57,7 @@ const dragons = [
     },
     {
         type: "Unconventional",
-        subtype: "Non-Aquatic",
+        subtype: "nonaquatic",
         name: "Mountain Goliath",
         info: "Gigantic sized dragon. Closely related to the Forest Goliath, they live in simliar habitats, though they are far more likely to be found in mountains than their counterpart. They have two pairs of wings, a smaller secondary pair located directly underneath the main pair.",
         imageMain: "resources/dragon_images/wings.png",
@@ -65,7 +65,7 @@ const dragons = [
     },
     {
         type: "Unconventional",
-        subtype: "Aquatic",
+        subtype: "aquatic",
         name: "Leviathan",
         info: "Large sized dragon. Little is known about this variety of dragon, as they are primarily found in the deep ocean. They have bioluminescent spots, and there are reports of them surfacing during violent storms.",
         imageMain: "resources/dragon_images/leviathan.png",
@@ -73,7 +73,7 @@ const dragons = [
     },
     {
         type: "Unconventional",
-        subtype: "Aquatic",
+        subtype: "aquatic",
         name: "Guidewind",
         info: "Small sized dragon. Despite being designated as an aquatic dragon, this species lives almost entirley in the air, only coming to land to raise their young and occasionally feed. They survive almost entirely off of arieal creatures such as birds. It is suspected that they share a common ancestor with Leviathans.",
         imageMain: "resources/dragon_images/guide1.png",
@@ -93,7 +93,7 @@ const dragons = [
 const conLight = [ 
     {
         name: "Mountain Red",
-        info: "Medium sized dragon. Its scales are various shades of reds, ranging from dark to light and anything in between. They have tusks located just underneath the eyes, and are capable of breathing fire. Their large wings enable them to soar well, and can often be spotted flying through the canyons of their natural mountainous habitat. Excellent hunters, they can easily hunt far more prey than hey need, which leads to a more social behavior compared to other dragons.",
+        info: "Medium sized dragon. Its scales are various shades of reds, ranging from dark to light and anything in between. They have tusks located just underneath the eyes, and are capable of breathing fire. Their large wings enable them to soar well, and can often be spotted flying through the canyons of their natural mountainous habitat. Excellent hunters, they can easily hunt far more prey than they need, which leads to a more social behavior compared to other dragons.",
         imageMain: "resources/dragon_images/quincy.png",
         image2: ""
     },
@@ -113,7 +113,7 @@ const conHeav = [
         image2: ""
     },
     {
-        name: "still dont have a name,,,,,",
+        name: "Lightning Flier",
         info: "Medium sized dragon. These dragons build up static and can use it as an elemental attack. Capable of flight, though they may prefer to stay on the ground and within caves.",
         imageMain: "resources/dragon_images/alphys_scene.jpg",
         image2: ""
@@ -150,15 +150,15 @@ const unconNonAq = [
         image2: ""
     },
     {
-        name: "Cave-Settler",
-        info: "Small sized dragon. Its wings are diaphanous, visually similar to insect wings. These dragons can be found within large cave systems and caverns, though they are known to surface at night. Particularly curious ones may be found at the edges of human settlements, investigating the people and buildings.",
-        imageMain: "resources/dragon_images/queen.png",
-        image2: ""
-    },
-    {
         name: "Mountain Goliath",
         info: "Gigantic sized dragon. Closely related to the Forest Goliath, they live in simliar habitats, though they are far more likely to be found in mountains than their counterpart. They have two pairs of wings, a smaller secondary pair located directly underneath the main pair.",
         imageMain: "resources/dragon_images/wings.png",
+        image2: ""
+    },
+    {
+        name: "Cave-Settler",
+        info: "Small sized dragon. Its wings are diaphanous, visually similar to insect wings. These dragons can be found within large cave systems and caverns, though they are known to surface at night. Particularly curious ones may be found at the edges of human settlements, investigating the people and buildings.",
+        imageMain: "resources/dragon_images/queen.png",
         image2: ""
     }
 
